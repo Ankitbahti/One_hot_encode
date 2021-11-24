1337444c6f0a0fdac91038b8e676a20bedd84159
Ankitbahti <66414000+Ankitbahti@users.noreply.github.com>
2021-11-24 13:08:53 +0530

    Create Onehotencde.py

---
+++ Onehotencde.py
@@ -0,0 +1,20 @@
+def onehot(df,features):
+    df=df.copy()
+    for feature in features : 
+        cats=list(df.loc[:,feature].unique())
+        if len(cats)==1 :
+            continue 
+            
+        loc=df.columns.get_loc(feature) 
+        [df.insert(loc=loc,column='{}_{}'.format(feature,cat),value=0.0) for cat in cats[:-1]]  
+        for index in df.index :
+            val=df.loc[index,feature] 
+            clm='{}_{}'.format(feature,val) 
+            if clm in df.columns :
+                df.loc[index,clm]=1.0 
+            
+        df=df.drop(columns=[feature]) 
+        
+    
+        
+    return df
